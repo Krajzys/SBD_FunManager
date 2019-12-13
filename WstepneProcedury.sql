cde993d3bea26a656d017a98c277f358ecef9555
Beata <rrrr@vp.pl>
2019-12-13 22:44:09 +0100

    procedury wstawiajace

--- WstepneProcedury.sql
+++ WstepneProcedury.sql
@@ -1,87 +1,111 @@
-create or replace procedure wstaw_ksiazke_nowy_tworca
+create or replace procedure wstaw_ksiazke
 (tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2, gdzie_dodac VARCHAR2, id_uzytkownika_z NUMBER)
 is
-declare
-    ile_pozycji number;
-    my_id_pozycja number;
-    ile_autorow number;
+    ile_pozycji number(7,2);
+    my_id_pozycja number(7,2);
+    ile_autorow number(7,2);
 begin
-select count(*) into ile_autorow from Autor where imie=imie_z and nazwisko=nazwisko_z;
-select count(*) into ile_pozycji from Pozycja p natural join autor a where tytul=tytul_z and imie=imie_z and nazwisko=nazwisko_z;
-if ile_autorow < 1 then
-    insert into Autor(imie, nazwisko) values(imie_z, nazwisko_z);
-end if;
-if ile_pozycji < 1 then
-    insert into Pozycja(id_pozycja, tytul, rok, typ) values(my_id_pozycja, tytul_z, rok_z, 'ksiazka');
-    /* Tutaj mysle ze moznaby np dodac kolumne data dodania, bo moze sie zdazyc ze dwie ksiazki maja ten sam tytul */
-    select id_pozycja into my_id_pozycja from Pozycja where tytul=tytul_z and rok=rok_z and typ='ksiazka'; 
-    insert into Ksiazka(id_pozycja) values(my_id_pozycja);
-    insert into Autor_Ksiazka(imie, nazwisko, id_pozycja) values(imie_z, nazwisko_z, my_id_pozycja);
-end if;
-case gdzie_dodac
-when 'oczekujace' then
-insert into Oczekujace(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
-when 'w_trakcie' then
-insert into W_trakcie(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
-when 'ukonczona' then
-insert into Ukonczona(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
-end;
-
-end;
-/
-declare
-    ile number;
-begin
-    select count(*) into ile from Pozycja where tytul='Harry Potter';
-    if ile > 0 then
-    else
-        dbms_output.put_line('else');
+    select count(*) into ile_autorow from Autor where imie=imie_z and nazwisko=nazwisko_z;
+    select count(*) into ile_pozycji from Pozycja p natural join autor a where tytul=tytul_z and imie=imie_z and nazwisko=nazwisko_z;
+    if ile_autorow < 1 then
+        insert into Autor(imie, nazwisko) values(imie_z, nazwisko_z);
     end if;
+    if ile_pozycji < 1 then
+        insert into Pozycja(tytul, rok, typ) values(tytul_z, rok_z, 'ksiazka');
+        /* Tutaj mysle ze moznaby np dodac kolumne data dodania, bo moze sie zdazyc ze dwie ksiazki maja ten sam tytul */
+        select id_pozycja into my_id_pozycja from Pozycja where tytul=tytul_z and rok=rok_z and typ='ksiazka'; 
+        insert into Ksiazka(id_pozycja) values(my_id_pozycja);
+        insert into Autor_Ksiazka(imie, nazwisko, id_pozycja) values(imie_z, nazwisko_z, my_id_pozycja);
+    end if;
+    case gdzie_dodac
+    when 'oczekujace' then
+        insert into Oczekujace(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'w_trakcie' then
+        insert into W_trakcie(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'ukonczona' then
+        insert into Ukonczona(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    end
 end;
 /
-set serveroutput on;
-/
-select count(*) from Pozycja where tytul='Harry Potter';
-/
-select count(*) from Pozycja p natural join autor a where tytul='Harry Potter i Komnata Tajemnic' and imie='J.K' and nazwisko='Rowling';
-/
-select * from pozycja;
-/
-create or replace procedure wstaw_nowy_film_nowy_tworca
-(tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2)
+create or replace procedure wstaw_film
+(tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2, gdzie_dodac VARCHAR2, id_uzytkownika_z NUMBER)
 is
+    ile_pozycji number(7,2);
+    my_id_pozycja number(7,2);
+    ilu_rezyserow number(7,2);
 begin
-insert into Pozycja(id_pozycja, tytul, rok, typ) values(znalezione_id, tytul_z, rok_z, 'film');
-insert into Film();
-insert into Rezyser(imie, nazwisko) values(imie_z, nazwisko_z);
-insert into Rezyser_Film(imie, nazwisko) values(imie_z, nazwisko_z);
+    select count(*) into ilu_rezyserow from Rezyser where imie=imie_z and nazwisko=nazwisko_z;
+    select count(*) into ile_pozycji from Pozycja p natural join rezyser r where tytul=tytul_z and imie=imie_z and nazwisko=nazwisko_z;
+    if ilu_rezyserow < 1 then
+        insert into Rezyser(imie, nazwisko) values(imie_z, nazwisko_z);
+    end if;
+    if ile_pozycji < 1 then
+        insert into Pozycja(tytul, rok, typ) values(tytul_z, rok_z, 'film');
+        select id_pozycja into my_id_pozycja from Pozycja where tytul=tytul_z and rok=rok_z and typ='film';
+        insert into Film(id_pozycja) values(my_id_pozycja);
+        insert into Rezyser_Film(imie, nazwisko, id_pozycja) values(imie_z, nazwisko_z, my_id_pozycja);
+    end if;
+    case gdzie_dodac
+    when 'oczekujace' then
+        insert into Oczekujace(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'w_trakcie' then
+        insert into W_trakcie(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'ukonczona' then
+        insert into Ukonczona(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    end
 end;
 /
-create or replace procedure wstaw_nowy_serial_nowy_tworca
-(tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2)
+create or replace procedure wstaw_serial
+(tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2, gdzie_dodac VARCHAR2, id_uzytkownika_z NUMBER)
 is
+    ile_pozycji number(7,2);
+    my_id_pozycja number(7,2);
+    ilu_rezyserow number(7,2);
 begin
-insert into Pozycja(id_pozycja, tytul, rok, typ) values(znalezione_id, tytul_z, rok_z, 'serial');
-insert into Serial();
-insert into Rezyser(imie, nazwisko) values(imie_z, nazwisko_z);
-insert into Rezyser_Serial(imie, nazwisko) values(imie_z, nazwisko_z);
+    select count(*) into ilu_rezyserow from Rezyser where imie=imie_z and nazwisko=nazwisko_z;
+    select count(*) into ile_pozycji from Pozycja p natural join rezyser r where tytul=tytul_z and imie=imie_z and nazwisko=nazwisko_z;
+    if ilu_rezyserow < 1 then
+        insert into Rezyser(imie, nazwisko) values(imie_z, nazwisko_z);
+    end if;
+    if ile_pozycji < 1 then
+        insert into Pozycja(tytul, rok, typ) values(tytul_z, rok_z, 'serial');
+        select id_pozycja into my_id_pozycja from Pozycja where tytul=tytul_z and rok=rok_z and typ='serial';
+        insert into Serial(id_pozycja) values(my_id_pozycja);
+        insert into Rezyser_Serial(imie, nazwisko, id_pozycja) values(imie_z, nazwisko_z, my_id_pozycja);
+    end if;
+    case gdzie_dodac
+    when 'oczekujace' then
+        insert into Oczekujace(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'w_trakcie' then
+        insert into W_trakcie(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'ukonczona' then
+        insert into Ukonczona(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    end
 end;
 /
-create or replace procedure wstaw_nowy_serial_sezonow_nowy_tworca
-(tytul_z VARCHAR2, rok_z DATE, imie_z VARCHAR2, nazwisko_z VARCHAR2, liczba_sezonow_z NUMBER)
+create or replace procedure wstaw_gre
+(tytul_z VARCHAR2, rok_z DATE, nazwa_z VARCHAR2, gdzie_dodac VARCHAR2, id_uzytkownika_z NUMBER)
 is
+    ile_pozycji number(7,2);
+    my_id_pozycja number(7,2);
+    ilu_producentow number(7,2);
 begin
-insert into Pozycja(id_pozycja, tytul, rok, typ) values(znalezione_id, tytul_z, rok_z, 'serial');
-insert into Serial(liczba_zesonow) values(liczba_sezonow_z);
-insert into Rezyser(imie, nazwisko) values(imie_z, nazwisko_z);
-insert into Rezyser_Serial(imie, nazwisko) values(imie_z, nazwisko_z);
-end;
-/
-create or replace procedure wstaw_nowa_gre_nowy_tworca_rok_zalozenia
-(tytul_z VARCHAR2, rok_z DATE, nazwa_z VARCHAR2, rok_zalozenia_z DATE)
-is
-begin
-insert into Pozycja(id_pozycja, tytul, rok, typ) values(znalezione_id, tytul_z, rok_z, 'gra');
-insert into Gra();
-insert into Producent(nazwa, rok_zalozenia) values(nazwa_z, rok_zalozenia_z);
+    select count(*) into ilu_producentow from Producent where nazwa=nazwa_z;
+    select count(*) into ile_pozycji from Pozycja p natural join producent p where tytul=tytul_z and nazwa=nazwa_z;
+    if ilu_producento < 1 then
+        insert into Producent(nazwa) values(nazwa_z);
+    end if;
+    if ile_pozycji < 1 then
+        insert into Pozycja(tytul, rok, typ) values(tytul_z, rok_z, 'gra');
+        select id_pozycja into my_id_pozycja from Pozycja where tytul=tytul_z and rok=rok_z and typ='gra';
+        insert into Gra(id_pozycja, nazwa) values(my_id_pozycja, nazwa_z);
+    end if;
+    case gdzie_dodac
+    when 'oczekujace' then
+        insert into Oczekujace(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'w_trakcie' then
+        insert into W_trakcie(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    when 'ukonczona' then
+        insert into Ukonczona(id_uzytkownika, id_pozycja) values(id_uzytkownika_z, my_id_pozycja);
+    end
 end;
